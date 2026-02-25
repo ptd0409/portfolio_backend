e95c0e8f17b1c6620b417639ea77e2872dd10cef
Dung Pham <dung.pt@drdigital.vn>
2026-02-25 17:38:21 +0700

    :sprakles: update table

--- app/models/project.py
+++ app/models/project.py
@@ -6,9 +6,11 @@ class Project(Base):
     __tablename__ = "project"
     id = Column(Integer, primary_key=True, index=True)
     slug = Column(String, unique=True, index=True, nullable=False)
+    title = Column(String, nullable=True)
+    description = Column(String, nullable=True)
     cover_image_url = Column(String, nullable=True)
     repo_url = Column(String, nullable=True)
     published_at = Column(DateTime(timezone=True), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False, onupdate=func.now())
-    tags = relationship(Tags, )
+    tags = relationship("Tag", secondary="project_tag", back_populates="project")
---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from app.db.base import Base
+from sqlalchemy import Column, String, Integer, DateTime, func
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(hash)
+    role = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=True, nullable=True, onupdate=func.now())
